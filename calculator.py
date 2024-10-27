6c387635d9c13affd0ca307d0efd6e5222abb6f7
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-27 16:44:02 +0000

    Added a display of the calculator

--- calculator.py
+++ calculator.py
@@ -1,11 +1,18 @@
 import sys
-from PyQt5.QtWidgets import (QWidget, QApplication, QLabel, QDesktopWidget)
+from PyQt5.QtWidgets import (QWidget, QApplication, QLabel, QDesktopWidget, 
+                             QGridLayout, QLineEdit)
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
 
 class Calculator(QWidget):
   def __init__(self):
     super().__init__()
     self.time_label = QLabel("I am a calculator", self)
+    self.display = QLineEdit(self)
+    self.display.setReadOnly(True)
+    self.display.setFixedHeight(50)
+    self.display.setAlignment(Qt.AlignRight)
+    self.display.setText("0")
     self.initUI()
     
   def initUI(self):
@@ -13,13 +20,14 @@ class Calculator(QWidget):
     self.resize(400, 600)
     self.setWindowIcon(QIcon("calculator.png"))
     
+
+    
     self.center()
     
   def center(self):
     screen = QDesktopWidget().availableGeometry().center()
     frame = self.frameGeometry()
     frame.moveCenter(screen)
-
 
 def main():
   app =QApplication(sys.argv)
